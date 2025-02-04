7dac22ba5b6fc29725c6920326ab65fac76bf556
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-02-04 15:36:46 +0800

    Connect domManipulation when dom is loaded

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./assets/styles/utils.css";
 import "./assets/styles/styles.css";
+import domManipulation from "./assets/script/domManipulation";
 
-
+document.addEventListener("DOMContentLoaded", domManipulation);
