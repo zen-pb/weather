dbb66254cdc04888c2239b6f4487817ca80c09dd
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-02-04 19:40:01 +0800

    Change to only 7 days

--- src/assets/script/weatherAPI.js
+++ src/assets/script/weatherAPI.js
@@ -2,7 +2,7 @@ export default async function weatherAPI(location) {
   const API_KEY = "MCAAZARF95RSVNWZQEZMYSZ3G";
 
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${API_KEY}&contentType=json`
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=us&key=${API_KEY}&contentType=json`
   );
 
   const data = await response.json();
