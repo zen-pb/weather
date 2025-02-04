144df2b0d603731d0eb539c4802df1a06c0762da
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-02-04 12:59:48 +0800

    Add fonts

--- webpack.common.js
+++ webpack.common.js
@@ -28,6 +28,10 @@ module.exports = {
         type: "asset/resource",
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
         use: {
