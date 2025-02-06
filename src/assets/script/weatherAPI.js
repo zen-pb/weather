ac8347410cd070d25a830e46891fb8a4cf5d73d7
zen-pb <111935236+zen-pb@users.noreply.github.com>
2025-02-06 09:22:59 +0800

    Add constraint to weatherAPI

--- src/assets/script/weatherAPI.js
+++ src/assets/script/weatherAPI.js
@@ -2,7 +2,7 @@ export default async function weatherAPI(location) {
   const API_KEY = "MCAAZARF95RSVNWZQEZMYSZ3G";
 
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=us&key=${API_KEY}&contentType=json`
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=us&elements=datetime%2CdatetimeEpoch%2CresolvedAddress%2Ctempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Chumidity%2Cprecipprob%2Cwindspeed%2Cconditions%2Cdescription%2Cicon&key=${API_KEY}&contentType=json`
   );
 
   const data = await response.json();
